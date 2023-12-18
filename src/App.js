a79135a2e008f1d4f83c9762312d646e6ef27d41
kimjaer <wofus0422@gmail.com>
2023-12-18 11:49:43 +0900

    usestate 기초

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import navijson from './data/gnb.json';
+
+function App() {
+  const [tabvar, settab] = useState(0)
+  
+  const myfun = (v) => {
+    alert(v)
+  }
+  return (
+    <div>
+      <ul className='d-flex'>
+        {
+          navijson.map((v,idx) => {
+            return ( //변수는 꼭 괄호껴야함
+              <>
+                <li onClick={function () { settab(idx) }}>{v.name}</li>
+                {tabvar === idx && <div>  {/* 전부 참이여야만 나옴. fauls인 경우 그냥 멈춰버림. */}
+                  {idx}
+                </div>
+                }
+              </>
+            )
+          })
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default App;
